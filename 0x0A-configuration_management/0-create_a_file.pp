0fd5db60f554e1c4743bcde85efd36b49d46ab24
taguma tawa <tagumatawa@gmail.com>
2023-11-24 14:53:59 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # create a file in temp
+
 file { '/temp/school':
-   content => 'I love Puppet',
-   mode    => '0744',
-   owner   => 'www-data',
-   group   => 'www-data',
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
